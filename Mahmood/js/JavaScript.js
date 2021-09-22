627d99527bbc2e46aeebcf4bbcfb9257cb1f1191
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-09-22 12:55:21 +0200

    js

--- Mahmood/js/JavaScript.js
+++ Mahmood/js/JavaScript.js
@@ -21,8 +21,54 @@ console.log("The most valuable Player of the match is " + Players[2]);
 
 var Alpha = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat","Ferrari",];
 
-document.write(Alpha.sort());
+document.write(Alpha.sort()); // sort Array elements alphabatically
 
 
-// Intermediate 2 
+// Intermediate 2.1
+
+var fruits = ["apple"," banana","kiwi"] // main Array
+var moreFruits = fruits.push("orange") // added another fruit at the end
+document.write("<br>")
+document.write(fruits);
+document.write("<br>")
+var lessfruits=fruits.pop("orange"); // removed the last one 
+document.write(fruits)
+
+// var moreFruits = new Array();
+// moreFruits = fruits.push("orange");
+// document.write(moreFruits); // added another one at the end
+
+// var lessFruits = new Array();
+// lessFruits = moreFruits.pop("orange");
+// document.write(lessFruits);
+
+
+// Intermediate 2.2
+document.write("<br>")
+var animals = ["monkey", "horse", "dog"]
+var reversed = animals.reverse(); // reversed the Array
+document.write(animals); 
+document.write("<br>") // new line
+var moreAnimals= animals.unshift("cat"); // added another animal at the beginning 
+document.write(animals);
+
+// intermediate 2.3
+document.write("<br>")
+var fruitList = ["mango","cherries","kiwi","grapes","pear","peach","orange","lemon"]
+document.write(fruitList[0])
+document.write("<br>")
+document.write(fruitList[1])
+document.write("<br>")
+document.write(fruitList[2])
+document.write("<br>")
+document.write(fruitList[3])
+document.write("<br>")
+document.write(fruitList[4])
+document.write("<br>")
+document.write(fruitList[5])
+document.write("<br>")
+document.write(fruitList[6])
+document.write("<br>")
+document.write(fruitList[7])
+document.write("<br>")
 
