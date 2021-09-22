655ac0ceaa86bf6c755856370f4fcadbe8cffb9b
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-09-22 11:08:42 +0200

    Basic 3

--- Mahmood/js/JavaScript.js
+++ Mahmood/js/JavaScript.js
@@ -1,3 +1,9 @@
-var a = "Hi my name is Martin"
+var a = "Hi my name is Martin" // Basic 2 
 console.log(a)
 document.write(a) //to show the result on Html
+
+
+// Basic 3 
+
+var age= 25;
+document.write(" and i am " + age + " Years old ")
