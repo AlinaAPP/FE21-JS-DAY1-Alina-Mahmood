defbd91dd3cf981023aba70e273ffad238bf8e23
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-09-22 11:33:52 +0200

    js

--- Mahmood/js/JavaScript.js
+++ Mahmood/js/JavaScript.js
@@ -6,7 +6,7 @@ document.write(a); //to show the result on Html
 // Basic 3 
 
 var age= 25;
-document.write(" and i am " + age + " Years old ");
+document.write(" and i am " + age + " Years old <br> ");
 
 
 // Basic 4
@@ -14,3 +14,15 @@ document.write(" and i am " + age + " Years old ");
 var Players = ["Martin","Thomas","Peter","Mathias","Niki"]
 
 console.log("The most valuable Player of the match is " + Players[2]);
+
+
+
+// Intermediate 1
+
+var Alpha = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat","Ferrari",];
+
+document.write(Alpha.sort());
+
+
+// Intermediate 2 
+
